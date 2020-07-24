800104db02d09098e27f4e237c06e8cdbf130f16
aizhiping <aizp@bankcomm.com>
2020-07-24 14:48:24 +0800

    代码三大插件

--- app/src/main/java/com/example/checkcodestyle/MainActivity.java
+++ app/src/main/java/com/example/checkcodestyle/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity {
+    private String a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println(a);
+        final int count=10 ;
+        for (int i = 0; i < count; i++) {
+            System.out.println(i);
+        }
+        new Thread(){
+           private int a = 0;
+            @Override
+            public void run() {
+                super.run();
+
+                while (a<count) {
+                    System.out.println(a);
+                    a++;
+                }
+                System.out.println("end");
+            }
+        }.start();
     }
 }
